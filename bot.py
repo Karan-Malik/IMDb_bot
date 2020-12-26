ca47c49bc38d76db125812acb233822bcdc78f2d
Karan-Malik <karanmalik2000@gmail.com>
2020-12-26 19:45:27 +0530

    First commit

--- bot.py
+++ bot.py
@@ -66,10 +66,11 @@ def main():
     dp.add_handler(MessageHandler(Filters.text, reply))
     dp.add_error_handler(error)
 
-    updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
-                          url_path="1476373283:AAHrdQE394_J8qd78J974y_AwVdwrsis1r0")
-    updater.bot.setWebhook('https://imdb-movie-bot.herokuapp.com/' + "1476373283:AAHrdQE394_J8qd78J974y_AwVdwrsis1r0")
+    #updater.start_webhook(listen="0.0.0.0",
+    #                      port=int(PORT),
+    #                      url_path="1476373283:AAHrdQE394_J8qd78J974y_AwVdwrsis1r0")
+    #updater.bot.setWebhook('https://imdb-movie-bot.herokuapp.com/' + "1476373283:AAHrdQE394_J8qd78J974y_AwVdwrsis1r0")
+    updater.start_polling()
     updater.idle()
 
 
